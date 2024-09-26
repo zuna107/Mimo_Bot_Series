39bb904c4273fadaf6f541f6ec298f1594c8dc42
zetsuna <112685109+zuna107@users.noreply.github.com>
2024-09-26 21:11:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,31 +3,21 @@ require("dotenv").config();
 module.exports = {
   token: process.env.TOKEN || '', 
   prefix: process.env.PREFIX || '!', 
-  clientId: process.env.CLIENT_ID || '1237728403336204329', 
-  guildId: process.env.GUILD_ID || '1146443187016179842',
+  clientId: process.env.CLIENT_ID || 'CLIENT_ID', 
+  guildId: process.env.GUILD_ID || 'GUILD_ID',
   dbUri: process.env.MONGO_URI || '',
   embedColor: process.env.COlOR || '#6a8bf2',
-  targetRoleID: process.env.TARGET_ROLE_ID || '1274322860592336979',
+  targetRoleID: process.env.TARGET_ROLE_ID || 'TARGET_ROLE_MUTE',
   allowedRoleIds: process.env.ALLOWED_ROLE_IDS?.split(',') || [
-    '997074542629507153', 
-    '997155520752390174', 
-    '997908438476652624', 
-    '1164230979699880064', 
-    '1018873244494856252',
-    '1146461791598153748'
+    'ALLOWED_ROLE_ID', 
+    'ALLOWED_ROLE_ID'
   ],
   excludedRoleIDs: process.env.EXCLUDED_ROLE_IDS?.split(',') || [
-    '997074542629507153', 
-    '997155520752390174', 
-    '997908438476652624', 
-    '1164230979699880064', 
-    '1018873244494856252',
-    '1146461791598153748'
+    'EXCLUDED_ROLE_ID', 
+    'EXCLUDED_ROLE_ID' 
   ],
   excludedChannelIds: process.env.EXCLUDED_CHANNEL_IDS?.split(',') || [
-    '1159004419413774436', 
-    '997157992501891172', 
-    '997494503910940692', 
-    '1152945745226375289'
+    'EXCLUDED_CHANNEL_ID', 
+    'EXCLUDED_CHANNEL_ID'
   ],
 };
