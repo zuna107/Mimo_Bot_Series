f43b7d9d046566ae16bcf215ee3cf413f01fde59
zetsuna <112685109+zuna107@users.noreply.github.com>
2024-09-26 21:19:26 +0700

    Update editcommandsHandler.js

--- handlers/editcommandsHandler.js
+++ handlers/editcommandsHandler.js
@@ -3,23 +3,18 @@
 module.exports = {
     name: 'messageUpdate',
     async execute(oldMessage, newMessage, client) {
-        // Periksa apakah pesan tersebut dari bot atau tidak
         if (oldMessage.author.bot) return;
 
         // Periksa apakah pesan sebelumnya dan sesudahnya memiliki prefix yang sama
         const prefix = require('../config').prefix;
         if (!oldMessage.content.startsWith(prefix) || !newMessage.content.startsWith(prefix)) return;
 
-        // Pisahkan argumen dari pesan yang diperbarui
         const newArgs = newMessage.content.slice(prefix.length).trim().split(/ +/);
 
-        // Dapatkan nama perintah dari pesan
         const commandName = newArgs.shift().toLowerCase();
 
-        // Temukan perintah yang sesuai dengan nama
         const command = client.commands.get(commandName);
 
-        // Jika perintah tidak ditemukan, keluar
         if (!command) return;
 
         try {
