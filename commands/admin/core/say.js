93138d0952fb598d609446a645fcb58d6947bf15
zetsuna <112685109+zuna107@users.noreply.github.com>
2024-09-26 20:45:07 +0700

    Update say.js

--- commands/admin/core/say.js
+++ commands/admin/core/say.js
@@ -6,21 +6,19 @@ module.exports = {
     name: 'say',
     description: 'Mengirim, mengedit, atau menghapus pesan.',
     async execute(message, args) {
-        // Check permission using handler
+        // Cek izin pengguna menggunakan handler
         if (!checkPermission(message)) {
-            return; // If user doesn't have permission, stop execution
+            return; 
         }
     
-        // Jika tidak ada argumen yang diberikan
         if (!args.length) {
             return sendEmbedErrorAndHelp(message, 'Argument [channel] and [message] is missing', getHelpEmbed('helpSay'));
         }
 
-        // Jika subcommand adalah "del"
+        // Jika subcommand "del"
         if (args[0] === 'del') {
-            args.shift(); // Menghapus subcommand dari args
+            args.shift(); 
 
-            // Ambil link pesan dari argumen pertama
             const messageLink = args.shift();
 
             if (!messageLink) {
@@ -37,7 +35,7 @@ module.exports = {
                 return sendEmbedErrorAndHelp(message,"Invalid message link.", getHelpEmbed('helpSayDelete'));
             }
 
-            // Cari channel berdasarkan ID
+            // golek channel berdasarkan ID
             const channel = message.guild.channels.cache.get(channelID);
 
             if (!channel) {
@@ -45,7 +43,7 @@ module.exports = {
                 return sendEmbedErrorAndHelp(message,'Channel with ID not found.', getHelpEmbed('helpSayDelete'));
             }
 
-            // Hapus pesan dengan message ID
+            // hapus pesan
             channel.messages.fetch(messageID)
                 .then(msg => {
                     msg.delete()
@@ -65,18 +63,17 @@ module.exports = {
                 });
 
         } else if (args[0] === 'edit') {
-            args.shift(); // Menghapus subcommand dari args
+            args.shift(); 
         
-            // Cek apakah command memiliki argumen yang cukup
             if (args.length < 2) {
                 return sendEmbedErrorAndHelp(message,'Format: m!say edit [bot message link] [new message]', getHelpEmbed('helpSayEdit'));
             }
         
-            const messageLink = args.shift(); // Ambil link pesan dari argumen pertama
-            const newMessage = args.join(' '); // Gabungkan sisa argumen menjadi pesan baru
+            const messageLink = args.shift(); 
+            const newMessage = args.join(' '); 
         
             try {
-                // Ambil ID pesan dari link yang diberikan
+                // get ID pesan
                 const match = messageLink.match(/\/(\d+)\/(\d+)$/);
         
                 if (!match) {
@@ -87,23 +84,20 @@ module.exports = {
                 const [_, channelID, messageId] = match;
         
                 try {
-                    // Ambil pesan dari channel berdasarkan ID pesan
                     const editedMessage = await message.channel.messages.fetch(messageId);
-        
-                    // Edit pesan
+                    
                     await editedMessage.edit(newMessage);
-        
-                    // Dapatkan permalink untuk pesan yang diedit
+
                     const permalink = editedMessage.url;
         
-                    // Kirim pesan dengan link ke pesan yang telah diedit
+                    // tembak pesan sek wes di edit
                     message.reply(`Message edited ${permalink}`);
                 } catch (error) {
                     if (error.code === 10008) {
                         // Pesan tidak ditemukan
                         return sendEmbedErrorAndHelp(message,'Invalid message link or message not found.', getHelpEmbed('helpSayEdit'));
                     } else if (error.code === 50005) {
-                        // Tidak dapat mengedit pesan dari pengguna lain
+                        // Tidak dapat mengedit pesan dari pengguna lain (tidak dari bot)
                         return sendEmbedErrorAndHelp(message,'Incorrect or invalid bot message link.', getHelpEmbed('helpSayEdit'));
                     } else {
                         // Kesalahan lain yang tidak diharapkan
@@ -116,11 +110,9 @@ module.exports = {
                 message.channel.send('An error occurred while trying to edit a message.');
             }
         } else {
-            //kembali ke perintah utama "say"
-            // Ambil channel ID dari argumen pertama
+            //balik ke perintah utama "say"
             const channelArg = args.shift();
         
-            // Cari channel berdasarkan ID atau mention channel
             const channel = message.guild.channels.cache.get(channelArg.replace(/\D/g, '')) || message.mentions.channels.first();
         
             if (!channel) return sendEmbedErrorAndHelp(message,'Channel with ID or mention invalid or not found.', getHelpEmbed('helpSay'));
@@ -133,15 +125,13 @@ module.exports = {
                 return sendEmbedErrorAndHelp(message,'Please enter a message or upload a picture.', getHelpEmbed('helpSay'));
             }
         
-            // Kirim pesan bersama dengan semua lampiran jika ada
+            // Kirim pesan bersama dengan semua lampiran nek ada
             channel.send({
-                content: hasText ? text : null, // Gunakan null jika teks tidak ditemukan
+                content: hasText ? text : null, // Gunakan null jika teks tidak ada
                 files: hasAttachments ? message.attachments.map(attachment => attachment) : null
             })
             .then(sentMessage => {
-                // Jika pesan berhasil dikirim, buat pesan yang berisi link pesan tersebut
                 const messageLink = `${sentMessage.url}`;
-                // Balas pesan dengan link pesan yang terkirim
                 message.reply(messageLink)
                     .catch(err => {
                         console.error(err);
